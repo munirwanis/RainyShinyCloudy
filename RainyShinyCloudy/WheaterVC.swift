9fdd39f16e7a1c9079f493415637e317269bbb32
Munir Wanis <fallen.angel.pp@gmail.com>
2017-05-01 16:56:34 -0300

    :construction: Create interface and datasource

---
+++ RainyShinyCloudy/WheaterVC.swift
@@ -0,0 +1,38 @@
+//
+//  WheaterVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Munir Wanis on 24/04/17.
+//  Copyright © 2017 Munir Wanis. All rights reserved.
+//
+
+import UIKit
+
+class WheaterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+}
+
